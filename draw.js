3885e58d9b17b0a6a700a5885ff89643f77df941
Nick Minor <nickminor1991@gmail.com>
2020-01-30 18:22:28 -0700

    added snake to projects

---
+++ draw.js
@@ -0,0 +1,39 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
+
+(function setup() {
+  snake = new Snake();
+  fruit = new Fruit();
+  fruit.pickLocation();
+
+  window.setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fruit.draw();
+    snake.update();
+    snake.draw();
+
+    if (snake.eat(fruit)) {
+      fruit.pickLocation();
+    }
+    snake.checkCollision();
+    document.querySelector('.score')
+      .innerText = snake.total;
+  }, 160);
+}());
+
+window.addEventListener('keydown', ((evt) => {
+  const direction = evt.key.replace('Arrow', '');
+  snake.changeDirection(direction);
+}))
+window.addEventListener("keydown", function (e) {
+  // space and arrow keys
+  if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+    e.preventDefault();
+  }
+}, false);
